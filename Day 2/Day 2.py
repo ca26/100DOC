b917b7a98aa2e530308412925077ee7505b8128f
ca26 <c.atluri16@gmail.com>
2023-10-04 09:59:12 -0400

    Added files

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -1,1 +1,8 @@
-print ("Day 2")
+print("Welcome to the tip calc")
+bill = float(input("What was the total bill? "))
+percent = float(input("What percent tip? 10, 12, 15? "))
+people = int(input("how many peolpe? "))
+percentnum = percent/100
+billtotal = (bill*percentnum)+(bill)
+split = round(float(billtotal/people), 2)
+print(f"Each person should pay ${split}")
